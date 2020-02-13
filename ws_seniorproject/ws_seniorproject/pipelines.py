6cf54ee248af9a0e217c47cc33de122db7e1fd2e
Sephora J <sephorejean@gmail.com>
2020-02-13 07:19:44 -0500

    Items inserted into container & db created

--- ws_seniorproject/ws_seniorproject/pipelines.py
+++ ws_seniorproject/ws_seniorproject/pipelines.py
@@ -5,7 +5,45 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
+import sqlite3
 class WsSeniorprojectPipeline(object):
+
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn = sqlite3.connect("myproducts.db")
+        self.curr = self.conn.cursor()
+
+    def create_table(self):
+        self.curr.execute("""DROP TABLE IF EXISTS myproducts_tb""")
+        self.curr.execute("""create table myproducts_tb(
+            product_name text,
+            product_sale_price_dollars text,
+            product_sale_price_cents text,
+            product_rating text,
+            product_number_of_reviews text
+            )""")
+
     def process_item(self, item, spider):
+        self.store_db(item)
         return item
+
+
+    def store_db(self, item):
+        self.curr.execute("""insert into myproducts_tb values(?, ?, ?, ?, ?)""", (
+         item['product_name'][0],
+         item['product_sale_price_dollars'][0],
+         item['product_sale_price_cents'][0],
+         item['product_rating'][0],
+         item['product_number_of_reviews'][0]
+        ))
+        self.conn.commit()
+
+
+# import sqlite3
+# class WsSeniorprojectPipeline(object):
+#      def process_item(self, item, spider):
+#             print("Pipeline: " + item[product_name][0])
+#             return item
--- ws_seniorproject/ws_seniorproject/spiders/project1.py
+++ ws_seniorproject/ws_seniorproject/spiders/project1.py
@@ -9,15 +9,21 @@ class Project1Spider(scrapy.Spider):
     start_urls = ['https://www.amazon.com/s?k=nike+shoes+men&i=fashion-womens-shoes&crid=155J3BB8KTU8M&sprefix=nike+shoes%2Cfashion-womens-shoes%2C469&ref=nb_sb_ss_i_6_10']
 
     def parse(self, response):
-         items = WsSeniorprojectItem()
-         title = response.css('span.a-size-base-plus.a-color-base.a-text-normal::text').extract()
-         dollars = response.css('span.a-price-whole::text').extract()
-         cents = response.css('span.a-price-fraction::text').extract()
-         rating = response.css('span.a-icon-alt::text')[4:].extract()
-         reviews_number = response.css('span.a-size-base::text').extract()
-         items['product_name'] = title
-         items['product_sale_price_dollars'] = dollars
-         items['product_sale_price_cents'] = cents
-         items['product_rating'] = rating
-         items['product_number_of_reviews'] = reviews_number
-         yield items
+            items = WsSeniorprojectItem()
+
+            all_div_quotes = response.css('div.a-section.a-spacing-medium.a-text-center')
+
+            for project1 in all_div_quotes:
+                title = project1.css('span.a-size-base-plus.a-color-base.a-text-normal::text').extract()
+                dollars = project1.css('span.a-price-whole::text').extract()
+                cents = project1.css('span.a-price-fraction::text').extract()
+                rating = project1.css('span.a-icon-alt::text').extract()
+                reviews_number = project1.css('span.a-size-base::text').extract()
+                
+                items['product_name'] = title
+                items['product_sale_price_dollars'] = dollars
+                items['product_sale_price_cents'] = cents
+                items['product_rating'] = rating
+                items['product_number_of_reviews'] = reviews_number
+
+                yield items
